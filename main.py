b545bf02fc3fa818ed97bd5f6a9bb5d8fa2406c0
Rajveer Gupta <rajveergupta1304@gmail.com>
2025-12-18 17:25:19 +0530

    add a delete api method in main.py

--- main.py
+++ main.py
@@ -78,3 +78,17 @@ def update_user(user_id: int, data: UserUpdate, db: Session = Depends(get_db)):
     db.commit()
 
     return {"message": "User updated successfully"}
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+
+    return {"message": "User deleted successfully"}
+
